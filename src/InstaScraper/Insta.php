4a0c2c2585c36eb57b137b7afe02ec08169a0dc3
Matt Grubb <mgrubb0507@gmail.com>
2019-08-08 16:55:22 -0400

    Update userAgent, remove gis token in header.

--- src/InstaScraper/Insta.php
+++ src/InstaScraper/Insta.php
@@ -349,8 +349,8 @@ class Insta
             $headers = ['cookie' => $cookies, 'referer' => Endpoints::BASE_URL . '/', 'x-csrftoken' => $session['csrftoken']];
         }
 
-        $headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36';
-        $headers['x-instagram-gis'] = '4b9db4899ba710aa305d316bc9fd4677';
+        $headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36';
+        //$headers['x-instagram-gis'] = '4b9db4899ba710aa305d316bc9fd4677';
         $headers['x-requested-with'] = 'XMLHttpRequest';
         return $headers;
     }
